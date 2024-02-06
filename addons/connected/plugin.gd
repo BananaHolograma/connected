88fdfb707f86d58f1037586aaf3c2118ef58526a
bananaholograma <bananaholograma@proton.me>
2024-02-06 10:56:23 +0000

    moved icons folder into connected addon  to avoid import errors on installation

--- addons/connected/plugin.gd
+++ addons/connected/plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 
 
 func _enter_tree():
-	add_custom_type("ActionEmitter", "Node", preload("res://addons/connected/action_emitter.gd"), preload("res://addons/icons/action-emitter.svg"))
-	add_custom_type("ActionListener", "Node", preload("res://addons/connected/action_listener.gd"), preload("res://addons/icons/action-listener.svg"))
+	add_custom_type("ActionEmitter", "Node", preload("res://addons/connected/action_emitter.gd"), preload("res://addons/connected/icons/action-emitter.svg"))
+	add_custom_type("ActionListener", "Node", preload("res://addons/connected/action_listener.gd"), preload("res://addons/connected/icons/action-listener.svg"))
 	add_autoload_singleton("ActionInteractor", "res://addons/connected/action_interactor_brain.gd")
 
 
